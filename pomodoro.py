72c549abf79c16d1472fab79b5cde2bc3ad23763
jagaimoGit <111671037+jagaimoGit@users.noreply.github.com>
2024-03-22 09:20:41 +0800

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -3,27 +3,52 @@ from win11toast import notify
 from winsound import PlaySound, SND_ALIAS, SND_ASYNC
 
 
-def update(soundPath, name="DEFAULT"):
-    global time, timeText    
-    if time > 0:
-        time -= 1
-        timerCall = root.after(1000, update, soundPath)
-    if time == 0:
-        root.after_cancel(timerCall)
-        PlaySound(soundPath, SND_ALIAS|SND_ASYNC)
-        notify("Pomodoro Timer", f"Timer from {name} is done!", audio={'silent': 'true'})
-    timeText = "%02d:%02d" % (int(time / 60), time % 60)
-    timeLabel.configure(text=timeText)
-    print(timeText)
+def startPomodoro():
+    global isRunning
+    isRunning = True
+    startButton.configure(state="disabled")
+    stopButton.configure(state="normal")
+    update()
 
-soundPath = "sounds/defaultsound.wav"
+def stopPomodoro():
+    global isRunning
+    startButton.configure(state="normal")
+    stopButton.configure(state="disabled")
+    isRunning = False
+
+def update():
+    global soundPath, name, time, timeText, isRunning
+    if isRunning:
+        if time > 0:
+            time -= 1
+            timeText = "%02d:%02d" % (int(time / 60), time % 60)
+            timeLabel.configure(text=timeText)
+            print(timeText)
+            timerLoop = root.after(1000, update)  # Schedule the next update
+        else:
+            # Timer is done
+            PlaySound(soundPath, SND_ALIAS | SND_ASYNC)
+            notify("Pomodoro Timer", f"Timer from {name} is done!", audio={'silent': 'true'})
+    else:
+        # Stop the timer
+        try:
+            root.after_cancel(timerLoop)
+        except:
+            print("Timer is stopped/not running")
 root = tk.Tk()
 
+isRunning = None
+soundPath = "sounds/defaultsound.wav"
 time = 10
+name = "aklsdfkljaslkjdf"
 timeText = "%02d:%02d" % (int(time / 60), time % 60)
 timeLabel = tk.Label(root, text=timeText)
 timeLabel.pack()
 
-timerCall = root.after(1000, update, soundPath)
+startButton = tk.Button(root, text="Start", command=startPomodoro)
+startButton.pack()
+stopButton = tk.Button(root, text="Stop", command=stopPomodoro, state="disabled")
+stopButton.pack()
+
 
 root.mainloop()
